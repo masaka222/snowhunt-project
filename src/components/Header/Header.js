bbdb25557cd4644252a1f3f98a237651b17cc2ae
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-10-06 13:07:35 -0400

    Header with Flexbox

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './Header.css';
+import IconMagnifyingGlass from '../../IconMagnifyingGlass';
+import logo from '../../assets/SnowHunt-logo.png';
 
 class Header extends Component {
   render() {
     return (
       <header className="header">
-        
+        <div className="header__navigation">
+          <button className="nav-btn"></button>
+          Navigation
+        </div>
+        <img src={logo} alt="Snowhunt logo" className="header__logo"/>
+        <div className="header__search">
+          Search
+          <div className="magnifying-icon">
+            <IconMagnifyingGlass 
+                    width="2rem" 
+                    height="2rem"
+                    fill="#999"
+            />
+          </div>
+        </div>
       </header>
     );
   }
