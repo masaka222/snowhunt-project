a7fea9770e6867d7c4421f7cc9ff11b877beb04a
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-10-07 04:32:13 -0400

    Text & button positioning in MainContent

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -5,7 +5,9 @@ class MainContent extends Component {
   render() {
     return (
       <div className="main-content">
-        Sierra Nevada
+        <p className="main-content__title">Sierra Nevada</p>
+        <p className="main-content__subtitle">Mountain Range, Spain</p>
+        <button className="main-content__button">View Details</button>
       </div>
     );
   }
