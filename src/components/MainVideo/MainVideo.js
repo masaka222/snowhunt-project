019a17ea13d9a75dc1cdc7e1dd804436e3f21c25
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-10-06 22:53:56 -0400

    Video placement

--- src/components/MainVideo/MainVideo.js
+++ src/components/MainVideo/MainVideo.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './MainVideo.css';
+import videomp4 from '../../assets/Mt_Baker.mp4';
+import videowebm from '../../assets/Mt_Baker.webm';
 
 class MainVideo extends Component {
   render() {
     return (
       <div className="main-video">
-        
+        <video className = "main-video__content" autoPlay muted loop>
+          <source src={videomp4} type="video/mp4"/>
+          <source src={videowebm} type="video/webm"/>
+          Your browser is not supported!
+        </video>
       </div>
     );
   }
