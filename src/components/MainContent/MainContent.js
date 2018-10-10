b184efecdad7429b1bc4ddf4f779edfdd33371a9
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-10-10 09:14:18 -0400

    First part of animation, complete

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -3,11 +3,21 @@ import './MainContent.css';
 
 class MainContent extends Component {
   render() {
+    /* Adds the class of animated to different id elements*/
+    function ani(){
+      document.getElementById('sierra').classList.add('animated');
+      //delay until the upperpart is finished
+      setTimeout(function() {
+        document.getElementById('nevada').classList.add('animated')
+        },100)
+    }
+
     return (
       <div className="main-content">
-        <p className="main-content__title">Sierra Nevada</p>
+        <p className="main-content__title" id = 'sierra'>Sierra</p>
+        <p className="main-content__title" id = 'nevada'>Nevada</p>
         <p className="main-content__subtitle">Mountain Range, Spain</p>
-        <button className="main-content__button">View Details</button>
+        <button className="main-content__button" onClick={ani}>View Details</button>
       </div>
     );
   }
