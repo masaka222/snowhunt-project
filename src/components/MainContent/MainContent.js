b62fbd6388db11f8bc4bae417e6e0a504d771c78
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-10-11 13:42:02 -0400

    Second animation - Invisible content - done

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -10,14 +10,30 @@ class MainContent extends Component {
       setTimeout(function() {
         document.getElementById('nevada').classList.add('animated')
         },100)
+
+      setTimeout(function() {
+        document.getElementById('sierra').classList.add('animated2')
+        },400)
+      
+      setTimeout(function() {
+        document.getElementById('nevada').classList.add('animated2')
+        },500)
+
+      setTimeout(function() {
+        document.getElementById('subtext').classList.add('animated2')
+        },500)
+
+      setTimeout(function() {
+        document.getElementById('button').classList.add('animated2')
+        },500)
     }
 
     return (
       <div className="main-content">
         <p className="main-content__title" id = 'sierra'>Sierra</p>
         <p className="main-content__title" id = 'nevada'>Nevada</p>
-        <p className="main-content__subtitle">Mountain Range, Spain</p>
-        <button className="main-content__button" onClick={ani}>View Details</button>
+        <p className="main-content__subtitle" id = "subtext">Mountain Range, Spain</p>
+        <button className="main-content__button" id="button" onClick={ani}>View Details</button>
       </div>
     );
   }
