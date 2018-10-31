bb76cc38a26fd71998c281dce23835dd37809204
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-10-31 12:50:24 -0400

    Comments and more clear code

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -3,65 +3,55 @@ import './MainContent.css';
 
 class MainContent extends Component {
   render() {
-    /* Adds the class of animated to different id elements*/
+    /* 
+    The function ani() is used to make all the necessary animations on the right time,
+    after the press of the button. How it makes that happen, is by putting different "animated"
+    classes to specific elements by ID, in a specific timeframe
+    */
     function ani(){
-      document.getElementById('sierra').classList.add('animated');
+      document.getElementById('sierra').classList.add('animated1BoxCoverText');
+
       //delay until the upperpart is finished
       setTimeout(function() {
-        document.getElementById('nevada').classList.add('animated')
-        },100)
+        document.getElementById('nevada').classList.add('animated1BoxCoverText')
+      },100)
 
       setTimeout(function() {
-        document.getElementById('sierra').classList.add('animated2')
-        },400)
+        document.getElementById('sierra').classList.add('animated2BoxEraseText')
+      },400)
       
       setTimeout(function() {
-        document.getElementById('nevada').classList.add('animated2')
-        },500)
-
-      setTimeout(function() {
-        document.getElementById('subtext').classList.add('animated2')
-        },500)
-
-      setTimeout(function() {
-        document.getElementById('button').classList.add('animated2')
-        },500)
+        document.getElementById('nevada').classList.add('animated2BoxEraseText')
+        document.getElementById('subtext').classList.add('animated2BoxEraseText')
+        document.getElementById('button').classList.add('animated2BoxEraseText')
+        document.getElementById('video-container').classList.add('animated3MoveLeftAndScale')
+      },500)
       
       setTimeout(function() {
-        document.getElementById('video-container').classList.add('animated3')
-        },500)
+        document.getElementById('cloudimage').classList.add('animated4CloudUp')
+      },1000)
       
       setTimeout(function() {
-        document.getElementById('cloudimage').classList.add('animated4')
-        },1000)
+        document.getElementById('video').classList.add('animated5VideoUp')
+      },1350)
+
+      setTimeout(function() {
+        document.getElementById('main-title').classList.add('animated6TextUp')
+      },1500)
+
+      setTimeout(function() {
+        document.getElementById('main-subtitle').classList.add('animated6TextUp')
+      },1600)
       
       setTimeout(function() {
-          document.getElementById('video').classList.add('animated5')
-          },1350)
+        document.getElementById('video-container').classList.add('animated7ContainerUp')
+        document.getElementById('body').classList.add('animated8BackgroundColorChange')
+        document.getElementById('gridimage').classList.add('animated9ImageAnimation')
+      },2100)
 
       setTimeout(function() {
-          document.getElementById('main-title').classList.add('animated6')
-          },1500)
-
-      setTimeout(function() {
-          document.getElementById('main-subtitle').classList.add('animated6')
-          },1600)
-      
-      setTimeout(function() {
-          document.getElementById('video-container').classList.add('animated7')
-          },2100)
-      
-      setTimeout(function() {
-          document.getElementById('body').classList.add('animated8')
-          },2100)
-
-      setTimeout(function() {
-          document.getElementById('gridimage').classList.add('animated9')
-          },2100)
-
-      setTimeout(function() {
-          document.getElementById('gridtext').classList.add('animated6')
-          },2500)
+        document.getElementById('gridtext').classList.add('animated6TextUp')
+      },2500)
     }
 
     return (
