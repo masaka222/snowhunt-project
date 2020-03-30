f4865c896283d53664642b77078d46ae02d9557c
Vasilis Georgoudis <masakaftw@hotmail.com>
2020-03-30 13:49:48 +0300

    Security updates

---
+++ build/precache-manifest.d5454d0a6f3c52587e8a96cc89158411.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "22ce1b43f643daff0208520a24507dd9",
+    "url": "/snowhunt-project/static/media/man-on-mountain2.22ce1b43.png"
+  },
+  {
+    "revision": "0b98b20cf8ca5060ce43ba5a3ef24bb9",
+    "url": "/snowhunt-project/static/media/clouds3.0b98b20c.png"
+  },
+  {
+    "revision": "f91f517af341e697ac05da4689680a8b",
+    "url": "/snowhunt-project/static/media/SnowHunt-logo.f91f517a.png"
+  },
+  {
+    "revision": "92231c035e9fbad5e129fa95b44c7620",
+    "url": "/snowhunt-project/static/media/Mt_Baker.92231c03.webm"
+  },
+  {
+    "revision": "55b7cd706b52c3a2e2afca548df59061",
+    "url": "/snowhunt-project/static/media/Mt_Baker.55b7cd70.mp4"
+  },
+  {
+    "revision": "891e8b62e50498dd53aa",
+    "url": "/snowhunt-project/static/js/runtime~main.891e8b62.js"
+  },
+  {
+    "revision": "ed69ebcdd666d5f08681",
+    "url": "/snowhunt-project/static/js/main.ed69ebcd.chunk.js"
+  },
+  {
+    "revision": "aae8fc2bc6427ccc4a18",
+    "url": "/snowhunt-project/static/js/1.aae8fc2b.chunk.js"
+  },
+  {
+    "revision": "ed69ebcdd666d5f08681",
+    "url": "/snowhunt-project/static/css/main.cfa02fff.chunk.css"
+  },
+  {
+    "revision": "b0a8e8afe84e885340db6862936838bc",
+    "url": "/snowhunt-project/index.html"
+  }
+];
