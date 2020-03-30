84ab5c34b65bf7bf1a69600f5d7e2fc862117dd3
Vasilis Georgoudis <masakaftw@hotmail.com>
2020-03-30 14:37:45 +0300

    Updates

---
+++ precache-manifest.6d597a74cc0effbabbf4a8bf2fdf662f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d598a2add88edb336f7fe107cf879f",
+    "url": "/snowhunt-project/index.html"
+  },
+  {
+    "revision": "ed81257752cdae456fd7",
+    "url": "/snowhunt-project/static/css/main.e48bf41f.chunk.css"
+  },
+  {
+    "revision": "39f1e17e45bf1c5f21ea",
+    "url": "/snowhunt-project/static/js/2.7d340d45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snowhunt-project/static/js/2.7d340d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed81257752cdae456fd7",
+    "url": "/snowhunt-project/static/js/main.26e95a64.chunk.js"
+  },
+  {
+    "revision": "d2200f2362e03ce7d178",
+    "url": "/snowhunt-project/static/js/runtime-main.1ae440a2.js"
+  },
+  {
+    "revision": "55b7cd706b52c3a2e2afca548df59061",
+    "url": "/snowhunt-project/static/media/Mt_Baker.55b7cd70.mp4"
+  },
+  {
+    "revision": "92231c035e9fbad5e129fa95b44c7620",
+    "url": "/snowhunt-project/static/media/Mt_Baker.92231c03.webm"
+  },
+  {
+    "revision": "f91f517af341e697ac05da4689680a8b",
+    "url": "/snowhunt-project/static/media/SnowHunt-logo.f91f517a.png"
+  },
+  {
+    "revision": "0b98b20cf8ca5060ce43ba5a3ef24bb9",
+    "url": "/snowhunt-project/static/media/clouds3.0b98b20c.png"
+  },
+  {
+    "revision": "22ce1b43f643daff0208520a24507dd9",
+    "url": "/snowhunt-project/static/media/man-on-mountain2.22ce1b43.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/snowhunt-project/precache-manifest.d5454d0a6f3c52587e8a96cc89158411.js"
+  "/snowhunt-project/precache-manifest.6d597a74cc0effbabbf4a8bf2fdf662f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/snowhunt-project/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/snowhunt-project/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
